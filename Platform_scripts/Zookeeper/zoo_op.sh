eec77bed738518a15d08741947d58569d5f80761
BryantChang <finish740@sina.com>
2018-03-29 20:00:40 +0800

    modify hadoop and zookeeper scripts

--- Platform_scripts/Zookeeper/zoo_op.sh
+++ Platform_scripts/Zookeeper/zoo_op.sh
@@ -8,6 +8,18 @@ function miss_env_file() {
     echo "missing the zoo_env.sh, you should rename the templet file to the zoo_env.sh"
 }
 
+function miss_zoo_home(){
+    echo "ZOO_HOME is not set, please set ZOO_HOME in the zoo_env.sh"
+}
+
+function miss_zoo_cfg(){
+    echo "missing zoo_cfg please create the zoo.cfg"
+}
+
+function miss_data_dir_cfg() {
+    echo "parameter dataDir is not set, please check your zoo.cfg!!"
+}
+
 if [[ $# -lt 1 ]]; then
     usage
     exit
@@ -27,6 +39,12 @@ fi
 
 . ${ZK_SCRIPT_DIR}/zoo_env.sh
 
+if [[ -z ${ZOO_HOME} ]]; then
+    miss_zoo_home
+    exit
+fi
+
+
 
 
 op_type=$1
@@ -45,9 +63,19 @@ for line in `cat ${ZK_SCRIPT_DIR}/serverlist`; do
         ssh ${hostname} "${ZOO_HOME}/bin/zkServer.sh stop"
         echo "succ"
     elif [[ ${op_type} = "init" ]]; then
-        myid=`echo ${line} | cut -d ':' -f 2`
+        if [[ ! -f ${ZOO_HOME}/conf/zoo.cfg ]]; then
+            miss_zoo_cfg
+            exit
+        fi
+        data_dir=`grep "dataDir=" ${ZOO_HOME}/conf/zoo.cfg | cut -d '=' -f 2`
+        if [[ -z ${data_dir} ]]; then
+            miss_data_dir_cfg
+            exit
+        fi
         echo "on ${hostname} initializing zk server..."
-        ssh ${hostname} "echo ${myid} > ${ZOO_HOME}/data/myid"
+        mkdir -p ${data_dir}
+        myid=`echo ${line} | cut -d ':' -f 2`
+        ssh ${hostname} "echo ${myid} > ${data_dir}/myid"
         echo "succ"
     fi
 done
